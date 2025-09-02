bce6507568d34cf20ba0483d2ad3c93911365270
zhangwenshuo <2452961693@qq.com>
2025-09-02 17:42:02 +0800

    通用

---
+++ src/utils/auth/token.js
@@ -0,0 +1,21 @@
+/**
+ * cookies缓存存储数据
+ * 方法：get、set、remove
+ * 
+ */
+import Cookies from 'js-cookie'
+
+const TokenKey = 'Admin-Token'
+
+export function getToken() {
+  return Cookies.get(TokenKey)
+}
+
+export function setToken(token) {
+  return Cookies.set(TokenKey, token)
+}
+
+export function removeToken() {
+  return Cookies.remove(TokenKey)
+}
+
