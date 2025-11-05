13f83e8a85a6c836712e7ce491dbb7acbada4a13
zhangsd <joker_zhang1120@163.com>
2025-11-05 14:27:42 +0800

    fix 角色管理修改字段显示

--- src/views/system/IframePageView/index.vue
+++ src/views/system/IframePageView/index.vue
@@ -1,21 +1,14 @@
 <!--
  * @Author: zhangsd
  * @Date: 2025-09-22 20:29:45
- * @LastEditTime: 2025-11-03 17:17:37
+ * @LastEditTime: 2025-11-05 10:49:46
  * @LastEditors: zhangsd
  * @Description: 积木报表设计 
  * @FilePath: \view\src\views\system\IframePageView\index.vue
 -->
 <template>
 	<div class="iframe-container" v-if="showIframe">
-		<iframe 
-			:id="id" 
-			:src="iframeUrl" 
-			frameborder="0" 
-			width="100%" 
-			height="800px" 
-			scrolling="auto"
-		></iframe>
+		<iframe :id="id" :src="iframeUrl" frameborder="0" width="100%" height="800px" scrolling="auto"></iframe>
 	</div>
 </template>
 
@@ -40,11 +33,11 @@ let openingMethod = '' // 存储打开方式参数值
 const getOpeningMethod = async () => {
 	try {
 		const res = await publicApi.getSystemParams({ key: 'REPORT_OPENING_METHOD' })
-		
+
 		if (res.code === '0000') {
 			openingMethod = res.data.paramVal
 			console.log('获取到报表打开方式参数:', openingMethod)
-			
+
 			// 根据参数值执行相应操作
 			if (openingMethod === 'Y') {
 				// Y：外部打开 - 新窗口打开
@@ -81,23 +74,23 @@ const openInNewWindow = () => {
 	const token = getToken()
 	const tenantId = 'dev-api'
 	const newUrl = basePath + '/jmreport/list?token=' + encodeURIComponent(token) + '&tenantId=' + tenantId
-	
+
 	console.log('外部打开报表URL:', newUrl)
-	
+
 	// 新窗口打开
 	const newWindow = window.open(newUrl, '_blank', 'noopener,noreferrer')
-	
+
 	if (!newWindow) {
 		// 如果被浏览器阻止弹窗，提示用户
 		// ElMessage.warning('新窗口打开被浏览器阻止，请检查浏览器弹窗设置')
 	} else {
 		console.log('报表已在新窗口打开')
-		
+
 		// // 如果需要，可以询问用户是否关闭当前页面
 		// if (closeCurrent) {
 		// 	ElMessageBox.confirm(
-		// 		'报表已在新窗口打开，是否关闭当前页面？', 
-		// 		'提示', 
+		// 		'报表已在新窗口打开，是否关闭当前页面？',
+		// 		'提示',
 		// 		{
 		// 			confirmButtonText: '关闭',
 		// 			cancelButtonText: '保留',
@@ -119,9 +112,9 @@ const buildIframeUrl = () => {
 	id.value = path
 	const token = getToken()
 	const tenantId = 'dev-api'
-	
+
 	iframeUrl.value = basePath + '/jmreport/list?token=' + encodeURIComponent(token) + '&tenantId=' + tenantId
-	
+
 	console.log('构建iframe URL =>', iframeUrl.value)
 	console.log('------token------', encodeURIComponent(token))
 }
@@ -145,7 +138,7 @@ watch(
 )
 </script>
 
-<style scoped>
+<style  lang="scss">
 .iframe-container {
 	width: 100%;
 	height: 100%;
@@ -157,5 +150,11 @@ watch(
 	height: 800px;
 	border: none;
 	box-sizing: border-box;
+}
+:deep(.jimu-header) {
+	display: none !important;
+}
+::deep(.jimu-logo) {
+	display: none !important;
 }
 </style>
