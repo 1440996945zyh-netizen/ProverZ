1501c58517c28cda0f57d04688ec81a840d372f5
limian <1692717809@qq.com>
2026-02-26 16:59:05 +0800

    嵌套全生命周期和主控台

---
+++ src/views/equipment/equipmentLifeCycle/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="iframe-container">
+		<iframe src="/全生命周期.html" class="responsive-iframe" frameborder="0" allowfullscreen></iframe>
+	</div>
+</template>
+
+<style scoped>
+.iframe-container {
+	width: 100%;
+	height: 100vh; /* 占满视口高度 */
+	position: relative;
+}
+
+.responsive-iframe {
+	width: 100%;
+	height: 100%;
+	border: none;
+}
+</style>
