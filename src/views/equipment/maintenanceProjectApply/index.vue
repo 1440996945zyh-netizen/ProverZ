ff55bee982fba99d48e00feb421ca2bc8e6718f2
limian <1692717809@qq.com>
2026-03-17 10:25:33 +0800

    维修项目申请

--- src/views/equipment/maintenanceProjectApply/index.vue
+++ src/views/equipment/maintenanceProjectApply/index.vue
@@ -86,6 +86,24 @@ const tableColumns = ref([
 
 	{ label: '申请事项', prop: 'appContent', align: 'left', minWidth: 300, showOverFlow: true },
 	{
+		label: '是否作废',
+		prop: 'status',
+		align: 'center',
+		width: 100,
+		render: row => {
+			return h(ElTag, { type: row.status === '1' ? 'danger' : 'success' }, row.status === '1' ? '是' : '否')
+		},
+	},
+	{
+		label: '是否结算',
+		prop: 'isSettlement',
+		align: 'center',
+		width: 100,
+		render: row => {
+			return h(ElTag, { type: row.isSettlement === '1' ? 'success' : 'info' }, row.isSettlement === '1' ? '是' : '否')
+		},
+	},
+	{
 		label: '预算金额',
 		prop: 'budgetAmount',
 		align: 'right',
@@ -98,6 +116,7 @@ const tableColumns = ref([
 			return h('span', { style: { color: '#f56c6c', fontWeight: 'bold' } }, `¥${formattedValue}`)
 		},
 	},
+
 	{
 		label: '状态',
 		prop: 'processStatusLable',
@@ -281,6 +300,8 @@ const handleView = row => {
 			detailRef.value.formData.budgetAmount = resData.budgetAmount
 			detailRef.value.formData.remark = resData.remark
 			detailRef.value.formData.appType = resData.appType
+			detailRef.value.formData.status = resData.status
+			detailRef.value.formData.isSettlement = resData.isSettlement
 			detailRef.value.formData.list = resData.list || []
 			if (resData.list && resData.list.length > 0) {
 				detailRef.value.initQuotaTableData(resData.list)
@@ -310,6 +331,8 @@ const handleEdit = row => {
 			detailRef.value.formData.budgetAmount = resData.budgetAmount
 			detailRef.value.formData.remark = resData.remark
 			detailRef.value.formData.appType = resData.appType
+			detailRef.value.formData.status = resData.status
+			detailRef.value.formData.isSettlement = resData.isSettlement
 			detailRef.value.formData.list = resData.list || []
 			if (resData.list && resData.list.length > 0) {
 				detailRef.value.initQuotaTableData(resData.list)
