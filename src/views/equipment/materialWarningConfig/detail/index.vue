43c849e33b91af837ef78db5d17fd6cc7478b1f4
“zyh” <1440996945@qq.com>
2026-03-23 16:42:38 +0800

    调整物资预警配置功能新增时选择物资弹窗样式

--- src/views/equipment/materialWarningConfig/detail/index.vue
+++ src/views/equipment/materialWarningConfig/detail/index.vue
@@ -1,250 +1,276 @@
 <template>
-	<div class="formData">
-		<el-form :model="formData" ref="ruleForm" label-width="120px" :rules="rules">
-			<el-form-item label="物资" prop="materialId">
-				<el-input v-model="formData.materialName" placeholder="请选择物资" readonly @click="openMaterialSelect">
-					<template #suffix>
-						<el-icon style="cursor: pointer"><Search /></el-icon>
-					</template>
-				</el-input>
-			</el-form-item>
+  <div class="formData">
+    <el-form :model="formData" ref="ruleForm" label-width="120px" :rules="rules">
+      <el-form-item label="物资" prop="materialId">
+        <el-input v-model="formData.materialName" placeholder="请选择物资" readonly @click="openMaterialSelect">
+          <template #suffix>
+            <el-icon style="cursor: pointer"><Search /></el-icon>
+          </template>
+        </el-input>
+      </el-form-item>
 
-			
+      <el-form-item label="规格型号">
+        <el-input v-model="formData.specificationModel" disabled placeholder="自动带出" />
+      </el-form-item>
 
-			<el-form-item label="规格型号">
-				<el-input v-model="formData.specificationModel" disabled placeholder="自动带出" />
-			</el-form-item>
+      <el-form-item label="计量单位">
+        <el-input v-model="formData.unitName" disabled placeholder="自动带出" />
+      </el-form-item>
 
-			<el-form-item label="计量单位">
-				<el-input v-model="formData.unitName" disabled placeholder="自动带出" />
-			</el-form-item>
-			<el-form-item label="品牌">
-				<el-input v-model="formData.brand" disabled placeholder="自动带出" />
-			</el-form-item>
+      <el-form-item label="品牌">
+        <el-input v-model="formData.brand" disabled placeholder="自动带出" />
+      </el-form-item>
 
-			<el-form-item label="预警阈值" prop="warningThreshold">
-				<el-input-number
-					v-model="formData.warningThreshold"
-					:min="0"
-					:precision="2"
-					controls-position="right"
-					style="width: 100%"
-					placeholder="请输入预警阈值"
-				/>
-			</el-form-item>
+      <el-form-item label="预警阈值" prop="warningThreshold">
+        <el-input-number
+          v-model="formData.warningThreshold"
+          :min="0"
+          :precision="2"
+          controls-position="right"
+          style="width: 100%"
+          placeholder="请输入预警阈值"
+        />
+      </el-form-item>
 
-			<el-form-item label="预警接收人" prop="receivers">
-				<div class="receiver-wrapper">
-					<div class="selected-users">
-						<el-tag
-							v-for="user in selectedUsers"
-							:key="user.value"
-							closable
-							@close="removeUser(user)"
-							style="margin-right: 8px; margin-bottom: 8px"
-						>
-							{{ user.label }}
-						</el-tag>
-					</div>
-					<el-button type="primary" link @click="openUserSelect">
-						<el-icon><Plus /></el-icon>
-						选择人员
-					</el-button>
-				</div>
-			</el-form-item>
+      <el-form-item label="预警接收人" prop="receivers">
+        <div class="receiver-wrapper">
+          <div class="selected-users">
+            <el-tag
+              v-for="user in selectedUsers"
+              :key="user.value"
+              closable
+              @close="removeUser(user)"
+              style="margin-right: 8px; margin-bottom: 8px"
+            >
+              {{ user.label }}
+            </el-tag>
+          </div>
+          <el-button type="primary" link @click="openUserSelect">
+            <el-icon><Plus /></el-icon>
+            选择人员
+          </el-button>
+        </div>
+      </el-form-item>
 
-			<el-form-item label="状态" prop="status">
-				<el-radio-group v-model="formData.status">
-					<el-radio label="1">启用</el-radio>
-					<el-radio label="0">禁用</el-radio>
-				</el-radio-group>
-			</el-form-item>
-		</el-form>
+      <el-form-item label="状态" prop="status">
+        <el-radio-group v-model="formData.status">
+          <el-radio label="1">启用</el-radio>
+          <el-radio label="0">禁用</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
 
-		<el-dialog v-model="materialDialogVisible" title="选择物资" width="800px" append-to-body>
-			<el-form :inline="true" :model="materialQueryParams" style="margin-bottom: 16px">
-				<el-form-item label="物资名称">
-					<el-input v-model="materialQueryParams.materialName" placeholder="请输入物资名称" clearable />
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="getMaterialList">查询</el-button>
-					<el-button @click="resetMaterialQuery">重置</el-button>
-				</el-form-item>
-			</el-form>
-			<el-table
-				:data="materialTableData"
-				v-loading="materialLoading"
-				highlight-current-row
-				@current-change="handleMaterialCurrentChange"
-				style="width: 100%"
-				max-height="400"
-			>
-				<el-table-column type="index" width="50" label="序号" align="center" />
-				<el-table-column prop="materialName" label="物资名称" min-width="180" show-overflow-tooltip />
-				<el-table-column prop="specificationModel" label="规格型号" min-width="150" show-overflow-tooltip />
-				<el-table-column prop="brand" label="品牌" min-width="150" show-overflow-tooltip />
-				<el-table-column prop="unitName" label="计量单位" width="100" align="center" />
-			</el-table>
-			<el-pagination
-				v-model:current-page="materialQueryParams.startPage"
-				v-model:page-size="materialQueryParams.pageSize"
-				:page-sizes="[10, 20, 50]"
-				:total="materialTotal"
-				layout="total, sizes, prev, pager, next, jumper"
-				@size-change="getMaterialList"
-				@current-change="getMaterialList"
-				style="margin-top: 16px; justify-content: flex-end"
-			/>
-			<template #footer>
-				<el-button @click="materialDialogVisible = false">取消</el-button>
-				<el-button type="primary" @click="confirmMaterialSelect">确定</el-button>
-			</template>
-		</el-dialog>
+    <Dialog v-model:visible="materialDialogVisible" title="选择物资" width="70%" class="material-dialog">
+      <BaseTable
+        ref="materialTableRef"
+        :showSearchHeader="true"
+        :selectData="materialSelectData"
+        :searchClick="getMaterialList"
+        :tableColumns="materialTableColumns"
+        :tableData="materialTableData"
+        :loading="materialLoading"
+        :radioConfig="materialRadioConfig"
+        :total="materialTotal"
+        :show-pagination="true"
+        :tableHeight="tableHeight"
+      />
+      <template #footer>
+				<span class="dialog-footer">
+					<el-button @click="materialDialogVisible = false">取消</el-button>
+					<el-button type="primary" @click="confirmMaterialSelect">确定</el-button>
+				</span>
+      </template>
+    </Dialog>
 
-		<UserSelectForm ref="userSelectFormRef" @confirm="handleUserSelectConfirm" />
-	</div>
+    <UserSelectForm ref="userSelectFormRef" @confirm="handleUserSelectConfirm" />
+  </div>
 </template>
 
 <script setup name="materialWarningConfigDetail">
-import { ref, reactive, toRefs, getCurrentInstance } from 'vue'
+import { ref, reactive, toRefs, getCurrentInstance, computed, nextTick } from 'vue'
 import { Search, Plus } from '@element-plus/icons-vue'
 import UserSelectForm from '@/components/UserSelectForm/index.vue'
+import BaseTable from '@/components/BaseTable/index.vue'
+import Dialog from '@/components/Dialog/index.vue'
 import materialCodeApi from '@/api/equipment/materialCode/index'
+import tableParamsStore from '@/store/modules/tableParams'
 
 const { proxy } = getCurrentInstance()
+const tableHeight = computed(() => tableParamsStore().dialogPageTableHeight)
 
 const ruleForm = ref()
 const userSelectFormRef = ref()
+const materialTableRef = ref()
 const selectedUsers = ref([])
 
 const materialDialogVisible = ref(false)
 const materialLoading = ref(false)
 const materialTableData = ref([])
 const materialTotal = ref(0)
-const currentMaterialRow = ref(null)
 
 const materialQueryParams = ref({
-	startPage: 1,
-	pageSize: 10,
-	materialName: '',
+  startPage: 1,
+  pageSize: 20,
+  materialName: '',
 })
 
 const data = reactive({
-	formData: {
-		id: null,
-		materialId: null,
-		materialName: '',
-		brand: '',
-		specificationModel: '',
-		unitName: '',
-		warningThreshold: null,
-		receivers: '',
-		receiverNames: '',
-		status: '1',
-	},
+  formData: {
+    id: null,
+    materialId: null,
+    materialName: '',
+    brand: '',
+    specificationModel: '',
+    unitName: '',
+    warningThreshold: null,
+    receivers: '',
+    receiverNames: '',
+    status: '1',
+  },
 })
 const { formData } = toRefs(data)
 
 const rules = reactive({
-	materialId: proxy.getRules({ required: true, message: '请选择物资' }),
-	// warningThreshold: proxy.getRules({ required: true, message: '请输入预警阈值' }),
-	status: proxy.getRules({ required: true }),
+  materialId: proxy.getRules({ required: true, message: '请选择物资' }),
+  status: proxy.getRules({ required: true }),
+})
+
+const materialSelectData = reactive([
+  {
+    name: '物资名称',
+    type: 'input',
+    modelValue: 'materialName',
+    span: 12,
+  },
+])
+
+const materialTableColumns = ref([
+  { label: '', type: 'radio', width: 50, align: 'center' },
+  { label: '物资名称', prop: 'materialName', align: 'left', minWidth: 180, showOverFlow: true },
+  { label: '规格型号', prop: 'specificationModel', align: 'left', minWidth: 160, showOverFlow: true },
+  { label: '品牌', prop: 'brand', align: 'left', minWidth: 140, showOverFlow: true },
+  { label: '计量单位', prop: 'unitName', align: 'center', width: 120 },
+])
+
+const materialRadioConfig = reactive({
+  highlight: true,
+  strict: false,
+  trigger: 'row',
 })
 
 const openMaterialSelect = () => {
-	materialDialogVisible.value = true
-	getMaterialList()
+  materialDialogVisible.value = true
+  getMaterialList({ startPage: 1, pageSize: 20, materialName: '' })
 }
 
-const getMaterialList = () => {
-	materialLoading.value = true
-	materialCodeApi.getList(materialQueryParams.value).then(res => {
-		materialLoading.value = false
-		if (res.code === '0000') {
-			materialTableData.value = res.data.pages || []
-			materialTotal.value = res.data.totalNum || 0
-		}
-	})
-}
+const getMaterialList = params => {
+  materialLoading.value = true
+  materialQueryParams.value = {
+    ...materialQueryParams.value,
+    ...(params || {}),
+  }
 
-const resetMaterialQuery = () => {
-	materialQueryParams.value.materialName = ''
-	materialQueryParams.value.startPage = 1
-	getMaterialList()
-}
+  materialCodeApi
+    .getList(materialQueryParams.value)
+    .then(res => {
+      if (res.code === '0000') {
+        materialTableData.value = res.data?.pages || []
+        materialTotal.value = res.data?.totalNum || 0
 
-const handleMaterialCurrentChange = row => {
-	currentMaterialRow.value = row
+        nextTick(() => {
+          if (!materialTableRef.value || !formData.value.materialId) {
+            return
+          }
+
+          const currentRow = materialTableData.value.find(item => item.id === formData.value.materialId)
+          if (currentRow) {
+            materialTableRef.value.setRadioRow(currentRow)
+          }
+        })
+      } else {
+        materialTableData.value = []
+        materialTotal.value = 0
+        proxy.$message.error(res.msg || '获取物资列表失败')
+      }
+    })
+    .catch(() => {
+      materialTableData.value = []
+      materialTotal.value = 0
+      proxy.$message.error('获取物资列表失败')
+    })
+    .finally(() => {
+      materialLoading.value = false
+    })
 }
 
 const confirmMaterialSelect = () => {
-	if (!currentMaterialRow.value) {
-		proxy.$message.warning('请选择物资')
-		return
-	}
-	formData.value.materialId = currentMaterialRow.value.id
-	formData.value.materialName = currentMaterialRow.value.materialName
-	formData.value.brand = currentMaterialRow.value.brand
-	formData.value.specificationModel = currentMaterialRow.value.specificationModel || ''
-	formData.value.unitName = currentMaterialRow.value.unitName || ''
-	materialDialogVisible.value = false
+  const currentRow = materialTableRef.value?.getRadioRecord?.()
+
+  if (!currentRow) {
+    proxy.$message.warning('请选择物资')
+    return
+  }
+
+  formData.value.materialId = currentRow.id
+  formData.value.materialName = currentRow.materialName
+  formData.value.brand = currentRow.brand
+  formData.value.specificationModel = currentRow.specificationModel || ''
+  formData.value.unitName = currentRow.unitName || ''
+  materialDialogVisible.value = false
 }
 
 const openUserSelect = () => {
-	userSelectFormRef.value.open(formData.value.id, selectedUsers.value)
+  userSelectFormRef.value.open(formData.value.id, selectedUsers.value)
 }
 
 const handleUserSelectConfirm = (activityId, userList) => {
-
-	selectedUsers.value = userList || []
-	selectedUsers.value = userList
-	updateReceivers()
+  selectedUsers.value = userList || []
+  updateReceivers()
 }
 
 const removeUser = user => {
-	
-	selectedUsers.value = selectedUsers.value.filter(item => item.value !== user.id)
-	updateReceivers()
+  selectedUsers.value = selectedUsers.value.filter(item => item.value !== user.id)
+  updateReceivers()
 }
 
 const updateReceivers = () => {
-	formData.value.receivers = selectedUsers.value.map(item => item.value).join(',')
-	formData.value.receiverNames = selectedUsers.value.map(item => item.label).join(',')
+  formData.value.receivers = selectedUsers.value.map(item => item.value).join(',')
+  formData.value.receiverNames = selectedUsers.value.map(item => item.label).join(',')
 }
 
 const validate = async () => {
-	let flag = false
-	await ruleForm.value.validate((valid, fields) => {
-		if (valid) {
-			flag = true
-		} else {
-			flag = false
-			proxy.$message.warning('请完善表单信息！')
-		}
-	})
-	return flag
+  let flag = false
+  await ruleForm.value.validate(valid => {
+    if (valid) {
+      flag = true
+    } else {
+      flag = false
+      proxy.$message.warning('请完善表单信息！')
+    }
+  })
+  return flag
 }
 
 const resetForm = () => {
-	formData.value.id = null
-	formData.value.materialId = null
-	formData.value.materialName = ''
-	formData.value.brand = ''
-	formData.value.specificationModel = ''
-	formData.value.unitName = ''
-	formData.value.warningThreshold = null
-	formData.value.receivers = ''
-	formData.value.receiverNames = ''
-	formData.value.status = '1'
-	selectedUsers.value = []
-	ruleForm.value?.clearValidate()
+  formData.value.id = null
+  formData.value.materialId = null
+  formData.value.materialName = ''
+  formData.value.brand = ''
+  formData.value.specificationModel = ''
+  formData.value.unitName = ''
+  formData.value.warningThreshold = null
+  formData.value.receivers = ''
+  formData.value.receiverNames = ''
+  formData.value.status = '1'
+  selectedUsers.value = []
+  ruleForm.value?.clearValidate()
 }
 
 defineExpose({
-	validate,
-	resetForm,
-	formData,
-	selectedUsers,
+  validate,
+  resetForm,
+  formData,
+  selectedUsers,
 })
 </script>
 
@@ -252,10 +278,11 @@ defineExpose({
 @import '@/assets/styles/formData.scss';
 
 .receiver-wrapper {
-	width: 100%;
-	.selected-users {
-		min-height: 32px;
-		margin-bottom: 8px;
-	}
+  width: 100%;
+
+  .selected-users {
+    min-height: 32px;
+    margin-bottom: 8px;
+  }
 }
 </style>
